59f1e82fd6edab0d4865cd2936f105a160c98547
Cedric <crubbertire@rogers.com>
2025-12-23 17:47:56 -0500

    API post request for new company member creation

--- backend/src/controllers/AccountController.cs
+++ backend/src/controllers/AccountController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography;
-using System.Text;
 using Backend.Data;
 using Backend.Models;
 
@@ -10,8 +8,8 @@ namespace Backend.Controllers
   [ApiController]
   [Route("api/[controller]")] // The [controller] is a placeholder for the controller class below
                               // To derive the endpoint, we use the code below...
-  public class AccountController : ControllerBase // Controller Base takes 'UserController' and removes the suffix "Contoller" leaving it as 'User'.
-  {                                            // That is how we are left with the API endpoint of '/api/company'
+  public class AccountController : ControllerBase // Controller Base takes 'AccountController' and removes the suffix "Contoller" leaving it as 'User'.
+  {                                               // That is how we are left with the API endpoint of '/api/company'
     private readonly MyDbContext _context;
 
     public AccountController(MyDbContext context)
@@ -20,7 +18,7 @@ namespace Backend.Controllers
     }
 
 
-    //  POST '/api/account/register'
+    // Post Handling for '/api/account/register' (New Company)
     [HttpPost("register")]
     public async Task<ActionResult<Company>> Create([FromBody] AccountCreationDto dto) // Bind the request body to the client
     {
@@ -53,7 +51,7 @@ namespace Backend.Controllers
       {
         Name = dto.User.Name,
         Email = dto.User.Email,
-        Password_Hash =  passwordHash,
+        Password_Hash = passwordHash,
       };
 
       _context.Users.Add(user);
@@ -77,7 +75,7 @@ namespace Backend.Controllers
       {
         UserId = user.Id,
         CompanyId = company.Id,
-        Type =  Company_Member.MemberRoles.Owner,
+        Type = Company_Member.MemberRoles.Owner,
       };
 
       _context.Company_Members.Add(member);
@@ -93,11 +91,81 @@ namespace Backend.Controllers
       });
     }
 
+
+
+
+
+
+    // Post Handling for '/api/account/join' (New Member)
+    [HttpPost("join")]
+    public async Task<ActionResult<Company>> Create([FromBody] MemberCreationDto dto) // Bind the request body to the client
+    {
+
+      // Data comes in as follows:
+      // {
+      //   "User": {
+      //     "Name": string,
+      //     "Email": string,
+      //     "Password": string,
+      //   }, 
+      //   "Company": {
+      //     "Id": int,
+      //   }
+      //   "Member": {
+      //     "type": int,
+      //   }
+      // }
+
+      // Distinct Emails Only
+      if (await _context.Users.AnyAsync(user => user.Email == dto.User.Email))
+      {
+        return BadRequest("Email is already in use.");
+      }
+
+      // Hash password
+      string passwordHash = HashPassword(dto.User.Password);
+
+      // Create a new User
+      var user = new User
+      {
+        Name = dto.User.Name,
+        Email = dto.User.Email,
+        Password_Hash = passwordHash,
+      };
+
+      _context.Users.Add(user);
+      await _context.SaveChangesAsync(); // Insert user into DB, Generate the Id, Insert Id back into user
+
+
+
+      // Add user to company (Assign as Owner)
+      var member = new Company_Member
+      {
+        UserId = user.Id,
+        CompanyId = dto.Company.Id,
+        Type = (Company_Member.MemberRoles)dto.Company_Member.Type,
+      };
+
+      _context.Company_Members.Add(member);
+      await _context.SaveChangesAsync();
+
+
+
+      return Ok(new // HTTP 200 status code
+      {
+        Message = "Account successfully created",
+        UserId = user.Id,
+        CompanyMemberId = member.Id
+      });
+    }
+
+
+    // Function to hash passwords with BCrypt
     private string HashPassword(string password)
     {
       // BCrypt password hasher, provided password and salt amnt
       return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 12);
     }
-
   }
+
 }
--- backend/src/models/DTOs/AccountCreationDto.cs
+++ backend/src/models/DTOs/AccountCreationDto.cs
@@ -1,8 +1,58 @@
+// Data Transfer Object for Account Registration/Creation
+
+// When a user wants to create a new account/company, a Post request comes in like:
+//{
+//  "User": {
+//     "Name": "Stephen",
+//     "Email": "sLiu@hotmail.com",
+//     "Password": "i32312tge"
+//   }, 
+//   "Company": {
+//     "Name": "CBU",
+//     "Type": 1
+//   }
+//}
+// Therefore, we need a DTO to handle the User and Company objs
+
 public class AccountCreationDto
 {
   public UserDto User { get; set; } = null!;
   public CompanyDto Company { get; set; } = null!;
 }
+
+
+// Data Transfer Object for Creating a new Member of a Company
+
+// When a user wants to join a team, a Post request comes in like:
+//{
+//  "User": {
+//     "Name": string,
+//     "Email": string,
+//     "Password": string
+//   }, 
+//   "Company": {
+//     "Id": int
+//   }
+//   "Member": {
+//     "Type": int
+//   }
+//}
+// Therefore, we need a DTO to handle the User, Company, and Member objs
+
+public class MemberCreationDto
+{
+  public UserDto User { get; set; } = null!;
+  public CompanyDto Company { get; set; } = null!;
+  public MemberDto Company_Member { get; set; } = null!;
+}
+
+
+
+
+
+
+
+// Models for the DTOs set above
 
 public class UserDto
 {
@@ -14,5 +64,11 @@ public class UserDto
 public class CompanyDto
 {
   public string Name { get; set; } = string.Empty;
+  public int Type { get; set; }
+  public int Id { get; set; }
+}
+
+public class MemberDto
+{
   public int Type { get; set; }
 }
