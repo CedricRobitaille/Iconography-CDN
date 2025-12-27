a2318d13a3e75a4fb9bbdce59e4f1724b9cb1665
Cedric <crubbertire@rogers.com>
2025-12-27 16:41:39 -0500

    GET icons by companyId

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22454dab0ef4dcd3083640563aebba2997369e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31aadf4f0358daf7ddcb527540b6b0472006c8bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/src/controllers/IconController.cs
+++ backend/src/controllers/IconController.cs
@@ -71,8 +71,24 @@ namespace Backend.Controllers
     }
 
 
-    [HttpGet]
+    // Get all icons
+    [HttpGet] 
     public async Task<ActionResult<IEnumerable<Icon>>> GetAll()
       => await _context.Icons.ToListAsync();
+
+
+    // Get company's icons
+    [HttpGet("{companyId}")]
+    public async Task<ActionResult<IEnumerable<Icon>>> GetByCompanyId(int companyId)
+    {
+      // We need to query the `Company_Icons` table to find where companyId match, and return the icon
+      var icons = await _context.Company_Icons
+        .Where(ci => ci.CompanyId == companyId)
+        .Select(ci => ci.Icon)
+        .ToListAsync();
+
+      if (icons.Count == 0) return NotFound();
+      return icons;
+    }
   }
 }
