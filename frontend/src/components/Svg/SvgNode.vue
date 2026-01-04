d3ce23aa1a6827376cc15980bcb8070dc957b061
Cedric <crubbertire@rogers.com>
2026-01-04 16:03:17 -0500

    Added nodes to SvgNode component

--- frontend/src/components/Svg/SvgNode.vue
+++ frontend/src/components/Svg/SvgNode.vue
@@ -2,11 +2,16 @@
 
 <script setup lang="ts">
   import { computed } from 'vue';
-  import type { treeNode, svgDescriptor } from '../../types'; 
+  import type { treeNode } from '../../types'; 
 
   import SvgCircle from './Nodes/SvgCircle.vue';
   import SvgGroup from './Nodes/SvgGroup.vue';
   import SvgRect from './Nodes/SvgRect.vue';
+  import SvgEllipse from './Nodes/SvgEllipse.vue';
+  import SvgLine from './Nodes/SvgLine.vue';
+  import SvgPath from './Nodes/SvgPath.vue';
+  import SvgPolygon from './Nodes/SvgPolygon.vue';
+  import SvgPolyline from './Nodes/SvgPolyline.vue';
 
   const props = defineProps<{
     node: treeNode;
@@ -24,8 +29,12 @@
 
     <!-- Shapes -->
     <SvgCircle v-if="node.type === 'circle'" :node="node" />
+    <SvgEllipse v-if="node.type === 'ellipse'" :node="node" />
+    <SvgLine v-if="node.type === 'line'" :node="node" />
+    <SvgPath v-if="node.type === 'path'" :node="node" />
+    <SvgPolygon v-if="node.type === 'polygon'" :node="node" />
+    <SvgPolyline v-if="node.type === 'polyline'" :node="node" />
     <SvgRect v-if="node.type === 'rect'" :node="node" />
-    <!-- < v-if="node.type === ''" :node="node" /> -->
 
   </template>
 </template>
