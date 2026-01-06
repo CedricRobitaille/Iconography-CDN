7ede2b6ee97f635fa99e6192f0b8fa95bf5bc216
Cedric <crubbertire@rogers.com>
2026-01-05 19:24:02 -0500

    Color Module

--- frontend/src/components/PropertiesPanel/Color/Color.vue
+++ frontend/src/components/PropertiesPanel/Color/Color.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
-  import { computed, ref, toValue } from 'vue';
+  import { computed, ref, watch} from 'vue';
   import { useEditorStore } from '../../../stores/editorSvg';
-import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
+  import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
+  import { HSBtoHEX, HEXtoHSB } from '../../../composables/converter';
   const canvas = useEditorStore();
 
-  // The style we're currently editing
+  // Current Style (Fill or Stroke)
   const currentStyle = ref("fill")
   const setStyle = (style: string): void => {
     currentStyle.value = style;
   }
+
+  // HSB state for the picker
+  const hsb = ref({
+    h: 0 ,
+    s: 100,
+    b: 50,
+  });
+
+  // Whenever the HSB is changed (ie: picker/slider),
+  // Update the style in the pinia store.
+  watch(hsb, (newHSB) => {
+    const hex = HSBtoHEX(newHSB.h, newHSB.s, newHSB.b);
+    // Input converted HEX into state
+    if (currentStyle.value === "fill") {
+      canvas.activeStyle.fill.fill = sanitizeHex(hex);
+    } else {
+      canvas.activeStyle.stroke.stroke = sanitizeHex(hex);
+    }
+  }, { deep: true })
 
 
 
@@ -22,17 +42,20 @@ import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
       }
     },
     set: (value: string) => {
-      let val = sanitizeHex(value);
+      let hex = sanitizeHex(value);
 
       // Set the canvas with the new values
       if (currentStyle.value === "fill") {
-        canvas.activeStyle.fill.fill = val;
+        canvas.activeStyle.fill.fill = hex;
       } else {
-        canvas.activeStyle.stroke.stroke = val;
+        canvas.activeStyle.stroke.stroke = hex;
       }
+      // Update the HSB values
+      // hsb.value = HEXtoHSB(hex);
     }
   })
 
+  // User input to change opacity
   const opacityInput = computed({
     get: () => {
       // Get the opacity depending on current style
@@ -55,23 +78,87 @@ import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
     }
   });
 
+  // Change the hue on hue-slider change
+  const hueInput = computed({
+    get: () => {
+      return hsb.value.h
+    },
+    set: (val: number) => {
+      hsb.value.h = val;
+    }
+  })
+
+  // Enable colour picking on the colour picker
+  const startPicking = (event: PointerEvent) => {
+    const panel = event.currentTarget as HTMLElement;
+
+    const onMove = (event: PointerEvent) => {
+      const bounding = panel.getBoundingClientRect();
+      const x = Math.min(Math.max(event.clientX - bounding.left, 0), bounding.width)
+      const y = Math.min(Math.max(event.clientY - bounding.top, 0), bounding.height)
+
+      hsb.value.s = Math.round((x / bounding.width) * 100);
+      hsb.value.b = Math.round(100 - (y / bounding.height) * 100);
+    }
+
+    const onUp = () => {
+      window.removeEventListener('pointermove', onMove);
+      window.removeEventListener('pointerup', onUp);
+    }
+    window.addEventListener('pointermove', onMove);
+    window.addEventListener('pointerup', onUp);
+
+    onMove(event)
+  }
+
+  // Computed styling for the various page elements
+  const panelStyle = computed(() => ({
+    background: `
+        linear-gradient(to top, #000, transparent),
+        linear-gradient(to right, hsl(${hsb.value.h}, 0%, 100%), hsl(${hsb.value.h}, 100%, 50%))
+      `
+  }));
+
+  const cursorStyle = computed(() => ({
+    background: HSBtoHEX(hsb.value.h, hsb.value.s, hsb.value.b),
+  }));
+
+  const cursorPos = computed(() => ({
+    left: `${hsb.value.s - 5}%`,
+    top: `${100 - hsb.value.b - 5}%`
+  }))
 
 
-
+  // Instead of updating the HSB whenever HEX is changed, we'll do it here.
+  // That way, we allow greater UX, like
+  // Letting users put in partial inputs: ie: "#FF"
+  const updateHSBFromHEX = () => {
+    const hex = sanitizeHex(fillInput.value);
+    // If the user inputted all 6 chars
+    if (/^#([0-9A-Fa-f]{6})$/.test(hex)) {
+      hsb.value = HEXtoHSB(hex);
+    }
+  };
 
 </script>
 
 <template>
 
   <div id="picker-container">
-    <div id="color-picker">
-      <div id="picker-cursor"></div>
+    <div 
+      id="color-picker"
+      :style="panelStyle"
+      @pointerdown="startPicking"
+    >
+      <div id="picker-cursor" :style="cursorPos" >
+        <div id="picker-inner" :style="cursorStyle"></div>
+      </div>
     </div>
   </div>
 
   <div id="color-slider">
-    <input type="range" min="0" max="360" id="hue-slider">
-    <input type="range" min="0" max="100" id="opacity-slider">
+    <input type="range" min="0" max="360" id="hue-slider" v-model.number="hueInput">
+    <input type="range" min="0" max="100" id="opacity-slider" v-model.number="opacityInput">
   </div>
 
   <div id="color-select">
@@ -94,7 +181,7 @@ import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
       ></button>
     </div>
     <form>
-      <input type="text" class="color" v-model="fillInput" maxlength="7">
+      <input type="text" class="color" v-model="fillInput" maxlength="7" @blur="updateHSBFromHEX">
       <div class="opacity-container">
         <input type="number" class="opacity" v-model.number="opacityInput">
       </div>
@@ -116,14 +203,23 @@ import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
     position: relative;
     width: 100%;
     height: 9rem;
-    background: 
-      linear-gradient(to right, #fff, hsl(0, 100%, 50%));
     cursor: crosshair;
     border-radius: 4px;
+    
   }
 
   #picker-cursor {
     position: absolute;
+    border-radius: 16px;
+    border: 2px solid white;
+    box-shadow: 2px 2px 12px -4px #000000aa;
+  }
+
+  #picker-inner {
+    height: 16px;
+    width: 16px;
+    border-radius: 12px;
+    mix-blend-mode: normal;
   }
 
   #color-slider {
@@ -135,12 +231,59 @@ import { sanitizeHex, sanitizeOpacity } from '../../../composables/sanitizer';
   input[type="range"] {
     padding: 0;
     cursor: pointer;
+    -webkit-appearance: none;
+    appearance: none;
+    width: 100%;
+    height: .75rem;
+    border-radius: 1rem;
+    background: transparent;
+  }
+
+  #hue-slider {
+    background: linear-gradient(to right,
+      hsl(0, 80%, 50%),
+      hsl(60, 80%, 50%),
+      hsl(120, 80%, 50%),
+      hsl(180, 80%, 50%),
+      hsl(240, 80%, 50%),
+      hsl(300, 80%, 50%),
+      hsl(360, 80%, 50%));
+  }
+
+  #opacity-slider {
+    background: linear-gradient(to right,
+    rgba(255,255,255,0),
+    rgba(255,255,255,1)
+    );
+  }
+
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    border: 1.5px solid var(--bg-30);
+    background-color: var(--text-10); 
   }
 
   input[type="range"]:focus {
     outline: none;
   } 
 
+
+  /* Firefox */
+  #hue-slider input[type="range"]::-moz-range-track {
+    background: linear-gradient(
+      to right,
+      hsl(0, 100%, 50%),
+      hsl(60, 100%, 50%),
+      hsl(120, 100%, 50%),
+      hsl(180, 100%, 50%),
+      hsl(240, 100%, 50%),
+      hsl(300, 100%, 50%),
+      hsl(360, 100%, 50%)
+    );
+  }
 
 
   #color-select {
